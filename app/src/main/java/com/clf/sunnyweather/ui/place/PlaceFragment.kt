1140cff002cc733de97e7ce0421a23c4839327f0
clf <chenglingfeng@jte.com>
2022-02-10 18:07:51 +0800

    切换城市 手动刷新

--- app/src/main/java/com/clf/sunnyweather/ui/place/PlaceFragment.kt
+++ app/src/main/java/com/clf/sunnyweather/ui/place/PlaceFragment.kt
@@ -12,6 +12,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.clf.sunnyweather.MainActivity
 import com.clf.sunnyweather.R
 import com.clf.sunnyweather.ui.weather.WeatherActivity
 import kotlinx.android.synthetic.main.fragment_place.*
@@ -30,7 +31,7 @@ class PlaceFragment:Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        if(viewModel.isPlaceSaved()){
+        if(activity is MainActivity && viewModel.isPlaceSaved()){
             val place = viewModel.getSavedPlace()
             val intent = Intent(context,WeatherActivity::class.java).apply {
                 putExtra("location_lng",place.location.lng)
